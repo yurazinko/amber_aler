8769c7380e1b5eafd9a6f659a1732a139ed7d93e
Yurii Zinko <yurazinko@outlook.com>
2016-11-24 19:17:30 +0200

    update rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
-  resources :emergencies#, only: [:index, :show]
+  resources :emergencies, only: [:index, :show]
 end
