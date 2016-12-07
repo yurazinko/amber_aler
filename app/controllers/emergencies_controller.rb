42a9d8c33abac16c2639482dc9d698a7c3ce7cee
Yurii Zinko <yurazinko@outlook.com>
2016-12-07 15:21:53 +0200

    implement pagination for emergencies

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -19,7 +19,7 @@ class EmergenciesController < ApplicationController
   end
 
   def index
-    @emergencies = Emergency.all.order(updated_at: :desc).limit(5)
+    @emergencies = Emergency.all.order(updated_at: :desc).page(params[:page]).per(5)
   end
 
   def edit
